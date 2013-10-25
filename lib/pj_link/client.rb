45320d0d8a4e45bd9783e439b05385a4892a2b5a
Brian Goff <cpuguy83@gmail.com>
2013-10-25 10:26:22 -0400

    Add gateway class

--- lib/pj_link/client.rb
+++ lib/pj_link/client.rb
@@ -85,7 +85,75 @@ module PjLink
       command 'INFO ?'
     end
 
+    class Gateway
+      class ::PjLink::Client::PasswordRequiredError < StandardError; end
+      DEFAULT_PORT = 4352
+
+      attr_reader :connection, :connection_key, :port, :address
+
+      def initialize(address, port=DEFAULT_PORT)
+        @address, @port = address, port.to_i
       end
+
+      def send_message(message, password)
+        retry_count = 0
+        begin
+          connect(password)
+          @connection.write("#{@password_hash}%1#{message}\r")
+          response = @connection.waitfor("Prompt" => /%1/).
+            gsub(/^.*=/, "")
+
+          response
+        rescue Errno::EPIPE
+          if retry_count < 4
+            disconnect
+            retry_count += 1
+            retry
+          else
+            raise
+          end
+        rescue Net::ReadTimeout
+          raise PasswordRequiredError,
+            "You must provide a password to access this device"
+        ensure
+          disconnect
+        end
+      end
+    private
+      def connect(password)
+        retry_count = 0
+        begin
+          @connection = Net::Telnet.new(
+            'Host' => @address,
+            'Port' => @port,
+            'Telnetmode' => false,
+            'Promp' => /%1/)
+          _, auth_required, key = *@connection.sock.recvmsg[0].split
+        rescue Errno::ECONNRESET
+          if retry_count < 4
+            retry_count += 1
+            sleep 1
+            retry
+          else
+            raise
+          end
+        end
+
+        @password_hash = Digest::MD5.hexdigest("#{key}#{password}") if auth_required
+        self
+      end
+
+      def disconnect
+        begin
+          @connection.close if @connection
+        rescue IOError; nil
+        ensure
+          @connection = nil
+        end
+      end
+
     end
+
   end
 end
+
